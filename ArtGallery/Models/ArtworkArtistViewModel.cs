7cc7433419856f1c23cc0ed79d8b23e114176063
shupac800 <davidshuman@gmail.com>
2016-05-20 16:09:29 -0500

    displaying all works in inventory is working

---
+++ ArtGallery/Models/ArtworkArtistViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArtGallery.Models
+{
+    public class ArtworkArtistViewModel
+    {
+
+        public string Title { get; set; }
+
+        public short NumberInInventory { get; set; }
+
+        public string Name { get; set; }
+
+        public short? NumberMade { get; set; }
+    }
+}
